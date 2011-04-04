217fa72231224c4a343aa05591c17c8dc94f22f6
therevillsgames <therevillsgames@gmail.com>
2011-04-04 05:03:38 +0000

    Added stubs to the other targets

--- src/diddy/native/diddy.html5.js
+++ src/diddy/native/diddy.html5.js
@@ -1,10 +1,12 @@
-function systemMillisecs(){
-	return new Date().getTime();
-}
+var diddy = new Object();
 
-function flushKeys(){
+diddy.systemMillisecs=function(){
+	return new Date().getTime();
+};
+
+diddy.flushKeys=function(){
 	for( var i = 0; i < 512; ++i )
 	{
 		bb_input_device.keyStates[i]=0;
 	}
-}
+};
---
+++ src/diddy/native/diddy.ios.cpp
@@ -0,0 +1,14 @@
+class diddy
+{
+	public:
+
+	// stub
+	static int systemMillisecs() {
+		return 0;
+	}
+
+	// stub
+	static int flushKeys() {
+		return 0;
+	}
+}
