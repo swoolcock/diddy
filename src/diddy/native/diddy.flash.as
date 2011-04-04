a26468b511e79bf1c312e21a61747bdcda099291
therevillsgames <therevillsgames@gmail.com>
2011-04-04 10:13:36 +0000

    Added showMouse and hideMouse functions... code from:
    http://www.monkeycoder.co.nz/Community/posts.php?topic=262&

--- src/diddy/native/diddy.flash.as
+++ src/diddy/native/diddy.flash.as
@@ -1,12 +1,26 @@
+import flash.ui.Mouse;
+
 class diddy
 {
-	static public function systemMillisecs():Number{
+	static public function systemMillisecs():Number
+	{
 		return (new Date).getTime();
 	}
 
-	static public function flushKeys():void{
+	static public function flushKeys():void
+	{
 		for( var i:int=0;i<512;++i ){
 			bb_input_device.keyStates[i]=0;
 		}
+	}
+	
+	static public function showMouse():void
+	{
+		Mouse.show();
+	}
+
+	static public function hideMouse():void
+	{
+		Mouse.hide();
 	}
 }
--- src/diddy/native/diddy.xna.cs
+++ src/diddy/native/diddy.xna.cs
@@ -1,7 +1,7 @@
 class diddy
 {
-    public static int systemMillisecs()
-    {
+	public static int systemMillisecs()
+	{
 		DateTime centuryBegin = new DateTime(1970, 1, 1);
 		DateTime currentDate = DateTime.Now;
 		long elapsedTicks = currentDate.Ticks - centuryBegin.Ticks;
@@ -9,22 +9,26 @@ class diddy
 
 		int millisecs = (int)elapsedSpan.TotalSeconds * 1000;
 
-        return millisecs;
-    }
+		return millisecs;
+	}
     
-    public static void flushKeys()
-    {
-	/* Does not work, no access to gxtkGame
-	Generated Monkey code:
-		#if !WINDOWS_PHONE
-		public static void Main(){
-			new gxtkGame().Run();
-		}
-		#endif
+	public static void flushKeys()
+	{
+		// this only works if you change the mojo.xna.cs:
+		// public class gxtkApp{
+		//        public static gxtkInput input; <---- ADD STATIC KEYWORD HERE (LINE 282 - Monkey V34b)
 	
 		for( int i=0;i<512;++i ){
-			gxtkGame.app.input.keyStates[i]&=0x100;
+			gxtkApp.input.keyStates[i]&=0x100;
 		}
-	*/
-    }
+	}
+	
+	public static void showMouse()
+	{
+		gxtkApp.game.IsMouseVisible=true;
+	}
+	public static void hideMouse()
+	{
+		gxtkApp.game.IsMouseVisible=false;
+	}
 }
