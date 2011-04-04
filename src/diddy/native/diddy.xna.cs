ab5738b8bf8577c5cc2d8ed7f125e8283b1b963e
therevillsgames <therevillsgames@gmail.com>
2011-04-04 09:39:45 +0000

    Added millisecs to C#. Tried flushkeys but no access :(

--- src/diddy/native/diddy.xna.cs
+++ src/diddy/native/diddy.xna.cs
@@ -2,11 +2,29 @@ class diddy
 {
     public static int systemMillisecs()
     {
-        return 0;
+		DateTime centuryBegin = new DateTime(1970, 1, 1);
+		DateTime currentDate = DateTime.Now;
+		long elapsedTicks = currentDate.Ticks - centuryBegin.Ticks;
+		TimeSpan elapsedSpan = new TimeSpan(elapsedTicks);
+
+		int millisecs = (int)elapsedSpan.TotalSeconds * 1000;
+
+        return millisecs;
     }
     
-    public static int flushKeys()
+    public static void flushKeys()
     {
-        return 0;
+	/* Does not work, no access to gxtkGame
+	Generated Monkey code:
+		#if !WINDOWS_PHONE
+		public static void Main(){
+			new gxtkGame().Run();
+		}
+		#endif
+	
+		for( int i=0;i<512;++i ){
+			gxtkGame.app.input.keyStates[i]&=0x100;
+		}
+	*/
     }
 }
