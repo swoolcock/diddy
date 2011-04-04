94384996c88b6e5ad9c8d1f548c28be15d645bb9
therevillsgames <therevillsgames@gmail.com>
2011-04-04 09:06:18 +0000

    Added code for Flushkeys and millisecs for GLFW Target

--- src/diddy/native/diddy.glfw.cpp
+++ src/diddy/native/diddy.glfw.cpp
@@ -1,14 +1,19 @@
+#include <time.h>
+
 class diddy
 {
 	public:
 
-	// stub
+	// only accurate to 1 second 
 	static int systemMillisecs() {
-		return 0;
+		time_t seconds;
+		seconds = time (NULL);
+		return seconds * 1000;
 	}
 
-	// stub
-	static int flushKeys() {
-		return 0;
+	static void flushKeys() {
+		for( int i=0;i<512;++i ){
+			app->input->keyStates[i]&=0x100;
+		}
 	}
-}
+};
