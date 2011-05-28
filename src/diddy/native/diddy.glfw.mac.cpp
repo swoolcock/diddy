2c96d8308b03027ec5e007c9e77ee1eb860bc665
therevillsgames <therevillsgames@gmail.com>
2011-05-28 04:39:00 +0000

    Added MacOS version of GLFW

---
+++ src/diddy/native/diddy.glfw.mac.cpp
@@ -0,0 +1,33 @@
+#include <time.h>
+
+class diddy
+{
+	public:
+
+	// only accurate to 1 second 
+	static int systemMillisecs() {
+		time_t seconds;
+		seconds = time (NULL);
+		return seconds * 1000;
+	}
+
+	static void flushKeys() {
+		for( int i=0;i<512;++i ){
+			app->input->keyStates[i]&=0x100;
+		}
+	}
+	
+	static int getUpdateRate() {
+		return app->updateRate;
+	}
+	
+	static void showMouse()
+	{
+		// Empty Stub
+	}
+	static void hideMouse()
+	{
+		// Empty Stub
+	}
+	
+};
