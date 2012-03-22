909ed2679ba731dc314fa3a70ba68bb5ad482868
therevillsgames <therevillsgames@gmail.com>
2012-03-22 08:02:11 +0000

    Removed old MonkeyMax stuff to make way for the updated version

--- monkeymax/modules/mojomax/mojomax.monkey
+++
@@ -1,8 +0,0 @@
-#If TARGET<>"bmax"
-#Error "MojoMax is not currently available for target '${TARGET}'."
-#End
-
-Import app
-'Import audio
-Import graphics
-'Import input
--- monkeymax/src/1 - CopyTrans.bat
+++
@@ -1,9 +0,0 @@
-rebuildall.exe
-
-set OLDDIR=%CD%
-
-ren "%OLDDIR%\trans\trans.build\stdcpp\main_winnt.exe" trans_winnt.exe
-
-COPY "%OLDDIR%\trans\trans.build\stdcpp\trans_winnt.exe" %OLDDIR%\..\bin\"
-
-pause
