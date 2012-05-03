d3042d7419726742e95479fbf69d86ba110a43f2
therevillsgames <therevillsgames@gmail.com>
2012-05-03 09:45:52 +0000

    Added experimental pss target files for Diddy and a workaround for the missing Discard method (TOFIX: when Mark adds it)

---
+++ src/diddy/native/diddy.pss.cs
@@ -0,0 +1,128 @@
+class diddy
+{
+	public static int systemMillisecs()
+	{
+		DateTime centuryBegin = new DateTime(1970, 1, 1);
+		DateTime currentDate = DateTime.Now;
+		long elapsedTicks = currentDate.Ticks - centuryBegin.Ticks;
+		TimeSpan elapsedSpan = new TimeSpan(elapsedTicks);
+
+		int millisecs = (int)elapsedSpan.TotalSeconds * 1000;
+
+		return millisecs;
+	}
+
+	public static void flushKeys()
+	{
+		for( int i=0;i<512;++i ){
+			gxtkApp._app._input._keyStates[i]&=0x100;
+		}
+	}
+	
+	public static int getUpdateRate()
+	{
+		return gxtkApp._app._updateRate;
+	}
+	
+	public static int getPixel(int x, int y)
+	{
+		return 0;
+	}	
+	public static void showMouse()
+	{
+	}
+	public static void hideMouse()
+	{
+	}
+	public static void setGraphics(int w, int h)
+	{
+	}
+	public static void setMouse(int x, int y)
+	{
+	}
+	public static void showKeyboard()
+	{
+	}
+	public static void launchBrowser(String address, String windowName)
+	{
+	}
+	public static void launchEmail(String email, String subject, String text)
+	{
+	}
+	public static float realMod(float value, float amount) {
+		return value % amount;
+	}
+	public static void startVibrate(int millisecs)
+	{
+	}
+	public static void stopVibrate()
+	{
+	}
+	
+	public static int getDayOfMonth()
+	{
+		DateTime d = DateTime.Now;
+		return d.Day;
+	}
+	
+	public static int getDayOfWeek()
+	{
+		DateTime d = DateTime.Now;
+		return (int)(d.DayOfWeek);
+	}
+	
+	public static int getMonth()
+	{
+		DateTime d = DateTime.Now;
+		return d.Month;
+	}
+	
+	public static int getYear()
+	{
+		DateTime d = DateTime.Now;
+		return d.Year;
+	}
+	
+	public static int getHours()
+	{
+		DateTime d = DateTime.Now;
+		return d.Hour;
+	}
+	
+	public static int getMinutes()
+	{
+		DateTime d = DateTime.Now;
+		return d.Minute;
+	}
+	
+	public static int getSeconds()
+	{
+		DateTime d = DateTime.Now;
+		return d.Second;
+	}
+	
+	public static int getMilliSeconds()
+	{
+		DateTime d = DateTime.Now;
+		return d.Millisecond;
+	}
+	
+	public static void startGps()
+	{
+	}
+	public static String getLatitiude()
+	{
+		return "";
+	}
+	public static String getLongitude()
+	{
+		return "";
+	}
+	public static void showAlertDialog(String title, String message)
+	{ }
+	public static String getInputString()
+	{
+		return "";
+	}
+	
+}
